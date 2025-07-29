8826221feff6716bf22d51884f9ae8558874e8d5
jihongeek <jihongeek@kakao.com>
2025-07-29 10:55:30 +0900

    Chore: 웹 브라우저에서 사용할 수 있게 모킹 코드 추가

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,25 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import Home from './pages/Home.tsx';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+async function enableMocking() {
+  if (process.env.NODE_ENV !== 'development') {
+    return;
+  }
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Home />} />
-      </Routes>
-    </BrowserRouter>
-  </StrictMode>
-);
+  const { worker } = await import('./mocks/browser');
+
+  // `worker.start()` returns a Promise that resolves
+  // once the Service Worker is up and ready to intercept requests.
+  return worker.start();
+}
+enableMocking().then(() => {
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </StrictMode>
+  );
+});
