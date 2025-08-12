00665bb2fc48648ab681d91ff595bafcd61a76a4
jihongeek <jihongeek@kakao.com>
2025-08-12 14:18:35 +0900

    Feat: 채팅 목록을 서버에서 불러오고, 실시간 메시지 수신 기능 추가

--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -1,45 +1,98 @@
-import { dummyChatInfoLists, dummyUserChats } from '../mocks/dummyChats';
 import { compareRecent } from '../utils/utils';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import type { ChatInfoList, Message } from '../types/chats';
 export default function Chat() {
-  const { chatInfoList } = dummyChatInfoLists.filter(({ id }) => id === 1)[0];
-  chatInfoList.sort(compareRecent);
-  const [nowReceiver, setNowReceiver] = useState({
-    id: chatInfoList[0].id,
-    name: chatInfoList[0].name,
-  });
+  const userId = localStorage.getItem('user_id');
+  const [nowChatInfoList, setNowChatInfoList] = useState<ChatInfoList>([]);
+  const [nowReceiver, setNowReceiver] = useState<{
+    id: number;
+    name: string;
+  }>();
+  const loadChatInfoList = async () => {
+    try {
+      const response = await fetch('/chatlist', {
+        method: 'POST',
+        body: JSON.stringify({ id: userId }),
+      });
+      const sortedChatInfoList = (await response.json()).sort(compareRecent);
+      if (!response.ok) throw Error(`${response.status}`);
+      setNowChatInfoList(sortedChatInfoList);
+      return sortedChatInfoList;
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  useEffect(() => {
+    loadChatInfoList().then((nowChatInfoList) => {
+      setNowReceiver(nowChatInfoList[0]);
+    });
+  }, []);
+  useEffect(() => {
+    if (!nowReceiver) {
+      return;
+    }
+    const evtSource = new EventSource(`/chat/${userId}/${nowReceiver.id}`);
+    evtSource.onmessage = (e) => {
+      setMessages(JSON.parse(e.data));
+    };
+    return () => evtSource.close();
+  }, [nowReceiver]);
+  const [nowMessageContent, setNowMessageContent] = useState('');
+  const sendMessage = async (receiverId: number) => {
+    if (!userId) return;
+    try {
+      await fetch('/chat', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          senderId: parseInt(userId),
+          receiverId,
+          content: nowMessageContent,
+        }),
+      });
+      setNowMessageContent('');
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  const [messages, setMessages] = useState<Message[]>([]);
 
   return (
     <>
       <div>
         <h2>채팅 목록</h2>
-        {chatInfoList.map(({ id, name, date }) => {
-          return (
-            <div key={id} onClick={() => setNowReceiver({ id, name })}>
-              <img src='http://cataas.com/cat?width=50' alt='' /> {name} <br />
-              {date}
-            </div>
-          );
-        })}
+
+        {nowChatInfoList &&
+          nowChatInfoList.map(({ id, name, date }) => {
+            return (
+              <div key={id} onClick={() => setNowReceiver({ id, name })}>
+                <img src='http://cataas.com/cat?width=50' alt='' /> {name}{' '}
+                <br />
+                {date}
+              </div>
+            );
+          })}
       </div>
-      <div>
-        <img src='http://cataas.com/cat?width=50' alt='' /> {nowReceiver.name}
-        <br />
-        {dummyUserChats
-          .filter(({ id }) => id === 1)[0]
-          .messages.filter(
-            ({ senderId, receiverId }) =>
-              senderId === nowReceiver.id || receiverId === nowReceiver.id
-          )
-          .map(({ date, receiverId, content }) => (
-            <div>
+      {nowReceiver && (
+        <div>
+          <img src='http://cataas.com/cat?width=50' alt='' /> {nowReceiver.name}
+          <br />
+          {messages.map(({ date, receiverId, content }) => (
+            <div key={`${date},${receiverId}`}>
               {receiverId !== nowReceiver.id && '*'} <br />
               {content} <br />
               {date}
             </div>
           ))}
-        <input type='text' name='' id='' /> <button>전송</button>
-      </div>
+          <input
+            type='text'
+            name='messageInput'
+            value={nowMessageContent}
+            onChange={(event) => setNowMessageContent(event.target.value)}
+          />{' '}
+          <button onClick={() => sendMessage(nowReceiver.id)}>전송</button>
+        </div>
+      )}
     </>
   );
 }
