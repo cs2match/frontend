b16922724448ea4d8305577384fb3df54681dbac
jihongeek <jihongeek@kakao.com>
2025-08-13 13:15:47 +0900

    Chore: 회원가입 페이지 라우팅 설정

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import Home from './pages/Home.tsx';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Profile from './pages/Profile.tsx';
 import Chat from './pages/Chat.tsx';
+import Register from './pages/Register.tsx';
 async function enableMocking() {
   if (process.env.NODE_ENV !== 'development') {
     return;
@@ -24,6 +25,7 @@ enableMocking().then(() => {
           <Route path='/' element={<Home />} />
           <Route path='/chat' element={<Chat />} />
           <Route path='/profile/:id' element={<Profile />} />
+          <Route path='/register' element={<Register />} />
         </Routes>
       </BrowserRouter>
     </StrictMode>
